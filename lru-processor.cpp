7dcb87c59c87fe3b2609be72d69f8b666d33b36d
Herbert Aruya <haruyacs@gmail.com>
2019-05-11 13:21:53 -0500

    FORMALITY

--- lru-processor.cpp
+++ lru-processor.cpp
@@ -10,41 +10,6 @@ LeastRecentlyUsedProcessor::LeastRecentlyUsedProcessor(){
 }
 
 BUS_SIGNAL LeastRecentlyUsedProcessor::execute(int action, int cycle, int& index, string address){
-	string tag = ""; 
-	int cache_index = -1; 
-	getTagAndIndex(tag, index, Bus::hexToBinary(address));
-
-	if(READ == action){
-		bool cache_read = false; 
-		for(int i = 0; i < NUMBER_OF_CACHES; i++){
-			int old_cycle = processor_cache[i].lastAccessCycle(index);
-			string current_tag = "";
-		       	if(-1 != old_cycle) current_tag = processor_cache[i].read(index, old_cycle);	
-
-			if(current_tag == tag){
-				cache_read = true; 
-				cache_index = i; 
-				break; 
-			}
-		}
-		if(!(cache_read)){
-			cache_index = 0; 
-			int index_age = processor_cache[cache_index].lastAccessCycle(index);
-
-			for(int i = 1; i < NUMBER_OF_CACHES; i++){
-				int prospective_index_age = processor_cache[i].lastAccessCycle(index);
-				if(prospective_index_age < index_age){
-					cache_index = i; 
-					index_age = prospective_index_age; 
-				}
-			}
-		}
-	}
-
-	if(WRITE == action){
-
-	}
-
 	return None; 
 }
 
